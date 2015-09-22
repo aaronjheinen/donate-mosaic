d5b64a9add1b34dac96b68ef16e4be6f2ce4cdaf
Aaron Heinen <aaronjheinen@gmail.com>
2015-09-21 21:14:43 -0500

    admin media

--- app/Http/Controllers/Admin/ViewController.php
+++ app/Http/Controllers/Admin/ViewController.php
@@ -22,7 +22,7 @@ class ViewController extends Controller
      */
     public function index()
     {
-        $set = Set::with('purchases', 'squares.purchase')->where('id' , 1)->first();
+        $set = Set::with('purchases', 'squares.purchase.media')->where('id' , 1)->first();
         $squares = Square::where('class', 'taken')->where('set_id', $set->id)->get();
         
         return view('admin.index', [ 'set' => $set, 'squares' => $squares ]);
@@ -47,7 +47,7 @@ class ViewController extends Controller
      */
     public function image()
     {
-        $set = Set::with('squares.purchase')->where('id' , 1)->first();
+        $set = Set::with('squares.purchase.media')->where('id' , 1)->first();
 
         return view('admin.image', [ 'set' => $set ]);
     }
