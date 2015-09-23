5058d1c275ac22c828c67a8d868b3aeb4b011541
Aaron Heinen <aaronjheinen@gmail.com>
2015-09-22 20:15:52 -0500

    fix for transparent images showing green background

--- resources/views/square/available.blade.php
+++ resources/views/square/available.blade.php
@@ -1,2 +1,2 @@
-<span id="square-{{$square->id}}" class="donate-box {{$square->class}} {{$square->status}}"></span>
+<span id="square-{{$square->id}}" class="donate-box {{$square->class}} {{$square->status}} no-image"></span>
               
